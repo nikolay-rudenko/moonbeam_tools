fc4c8ff96f9f783b5840f53e032cc8ccbc5b7dd0
nikolay-rudenko <37703436+nikolay-rudenko@users.noreply.github.com>
2021-01-19 22:12:05 +0200

    Created script v0.0.0

--- JpgToPngConverter/jpg_to_png_convector.py
+++ JpgToPngConverter/jpg_to_png_convector.py
@@ -1,5 +1,7 @@
 import pathlib
 import re
+import time
+import sys
 
 from pathlib import Path
 from PIL import Image
@@ -8,48 +10,61 @@ from os import walk
 # frm_folder = sys.argv[1]
 # in_folder = sys.argv[2]
 
-frm_folder = 'pokemons'
-in_folder = '2'
+from_dir = 'pic'
+into_dir = 'new_folder'
 
 
-def folder_creator(from_dir, into_dir):
-    fr_dir_exist = Path(from_dir).is_dir()
-    in_dir_exist = Path(into_dir).is_dir()
+class Convertor:
+    def __init__(self, frm_folder, in_folder):
+        self.from_dir = frm_folder
+        self.into_dir = in_folder
 
-    if not fr_dir_exist:
-        print('please enter right from_folder name or check if exist')
-    elif fr_dir_exist and not in_dir_exist:
-        pathlib.Path(f'./{into_dir}').mkdir(parents=True, exist_ok=True)
-    if fr_dir_exist and in_dir_exist:
-        return True
+    def folder_creator(self):
+        fr_dir_exist = Path(self.from_dir).is_dir()
+        in_dir_exist = Path(self.into_dir).is_dir()
 
-folder_creator(frm_folder, in_folder)
+        if not fr_dir_exist:
+            fr_dir_message = 'please enter right from_folder name or check if exist'
+            print(fr_dir_message)
+            return fr_dir_message
+
+        elif fr_dir_exist and not in_dir_exist:
+            pathlib.Path(f'./{self.into_dir}').mkdir(parents=True, exist_ok=True)
+            print(f'{self.into_dir} created')
+
+            return 'folder created'
+        if fr_dir_exist and in_dir_exist:
+            return True
+
+    @staticmethod
+    def imgConverter():
+        # getting all the file names in folder
+        _, _, filenames = next(walk(f'./{from_dir}'))
+
+        t0 = time.time()
+        for pic in filenames:
+            t2 = time.time()
+
+            # checking them for .jpg format
+            regex = r".jpg+$"
+            is_jpg = re.search(regex, pic)
+
+            if is_jpg is not None:
+                # print(f'\nconverting... {pic}')
+                im = Image.open(f'./{from_dir}/{pic}')
+                im.save(f'./{into_dir}/{pic[:-4]}.png')
+            else:
+                print(f'{pic} is not .jpg file')
+            t3 = time.time()
+            total = '%.2f' % (t3 - t2)
+            print(f'{pic} converted time: {total} seconds')
+
+        t1 = time.time()
+        total = '%.2f' % (t1 - t0)
+
+        print(f'\ntotal converted for: {total} seconds')
 
 
-def imgConverter():
-
-    # getting all the file names in folder
-    _, _, filenames = next(walk(f'./{frm_folder}'))
-    regex = r".jpg+$"
-    isJpg = re.search(regex, filenames[0])
-
-    for pic in filenames:
-        im = Image.open(f'./{frm_folder}/{pic}')
-        im.save(f'./{in_folder}/{pic[:-4]}.png')
-
-    # for pic in range(len(filenames)):
-    #     im = Image.open(f'./{frm_folder}/{pic}')
-    #     yield im.save(f'./{in_folder}/{pic[:-4]}.png')
-
-    # checking them for .jpg format
-    if isJpg != None:
-        print('converting [0] img')
-    else:
-        print('[0] is not .jpg')
-
-
-
-
-
-imgConverter()
-
+c = Convertor(from_dir, into_dir)
+c.folder_creator()
+c.imgConverter()
