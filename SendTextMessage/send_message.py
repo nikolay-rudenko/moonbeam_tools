6a04cc230279c7f45ca2814deab584495bdba156
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-01-24 19:00:56 +0200

    Created message sender

--- SendTextMessage/send_message.py
+++ SendTextMessage/send_message.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = 'ACa1c90d8ac0ff3b14602e7c2cb19d9ab8'
-auth_token = '1146eb3539517867ca0f1ce51f71d440'
+account_sid = '***'
+auth_token = '***'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    from_='whatsapp:+14155238886',
+    from_='whatsapp:***',
     body='Hello from python!',
-    to='whatsapp:+380638768742'
+    to='whatsapp:***'
 )
 
 print(message.sid)
