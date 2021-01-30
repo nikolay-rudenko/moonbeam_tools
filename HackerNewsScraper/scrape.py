7d9ca8afeaa1bf98d6ad69a6236cf841ef18d564
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-01-30 21:28:22 +0200

    Added scrape.py

--- HackerNewsScraper/scrape.py
+++ HackerNewsScraper/scrape.py
@@ -2,27 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
-res = requests.get('https://news.ycombinator.com/news')
-soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.storylink')
-subtext = soup.select('.subtext')
 
 def sort_story_by_votes(hnlist):
     return sorted(hnlist, key= lambda k: k['votes'], reverse=True)
 
-def create_custom_hn(links,subtext):
+def create_custom_hn(pages):
     hn = []
+    for page in pages:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        links = soup.select('.storylink')
+        subtext = soup.select('.subtext')
 
-    for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].get('href', None)
-        vote = subtext[idx].select('.score')
+        for idx, item in enumerate(links):
+            title = links[idx].getText()
+            href = links[idx].get('href', None)
+            vote = subtext[idx].select('.score')
 
-        if len(vote):
-            points = int(vote[0].getText().replace(' points', ''))
-            if points > 100:
-                hn.append({'title': title, 'link': href, 'votes': points})
+            if len(vote):
+                points = int(vote[0].getText().replace(' points', ''))
+                if points > 400:
+                    hn.append({'title': title, 'link': href, 'votes': points})
+        return sort_story_by_votes(hn)
 
-    return sort_story_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links,subtext))
+def check_pages(quantity):
+    pages = []
+
+    for page in range(1, quantity + 1):
+        pages.append(requests.get(f'https://news.ycombinator.com/news?p={quantity}'))
+
+    return pages
+
+for page_num in range(1,3):
+    print(f'Page #{page_num} --------------------------------------------------------------------------------------')
+    if len(create_custom_hn(check_pages(page_num))) > 0:
+        pprint.pprint(create_custom_hn(check_pages(page_num)))
+
+
+
+
+
+
+
+
