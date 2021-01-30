b16b02a69936cd138d3051423d9b8b29b0d4a5e4
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-01-30 22:13:29 +0200

    Added scrape.py

--- HackerNewsScraper/scrape.py
+++ HackerNewsScraper/scrape.py
@@ -2,46 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
+page = requests.get('https://news.ycombinator.com/news?p=1')
+soup = BeautifulSoup(page.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+page1 = requests.get('https://news.ycombinator.com/news?p=2')
+soup1 = BeautifulSoup(page1.text, 'html.parser')
+links1 = soup1.select('.storylink')
+subtext1 = soup1.select('.subtext')
+
+slink = links + links1
+ssubtext = subtext + subtext1
+
 
 def sort_story_by_votes(hnlist):
-    return sorted(hnlist, key= lambda k: k['votes'], reverse=True)
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
-def create_custom_hn(pages):
+
+def create_custom_hn(links, subtext):
     hn = []
-    for page in pages:
-        soup = BeautifulSoup(page.text, 'html.parser')
-        links = soup.select('.storylink')
-        subtext = soup.select('.subtext')
 
-        for idx, item in enumerate(links):
-            title = links[idx].getText()
-            href = links[idx].get('href', None)
-            vote = subtext[idx].select('.score')
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
 
-            if len(vote):
-                points = int(vote[0].getText().replace(' points', ''))
-                if points > 400:
-                    hn.append({'title': title, 'link': href, 'votes': points})
-        return sort_story_by_votes(hn)
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 100:
+                hn.append({'title': title, 'link': href, 'votes': points})
+
+    return sort_story_by_votes(hn)
 
 
-def check_pages(quantity):
-    pages = []
-
-    for page in range(1, quantity + 1):
-        pages.append(requests.get(f'https://news.ycombinator.com/news?p={quantity}'))
-
-    return pages
-
-for page_num in range(1,3):
-    print(f'Page #{page_num} --------------------------------------------------------------------------------------')
-    if len(create_custom_hn(check_pages(page_num))) > 0:
-        pprint.pprint(create_custom_hn(check_pages(page_num)))
-
-
-
-
-
-
-
-
+pprint.pprint(create_custom_hn(slink, ssubtext))
