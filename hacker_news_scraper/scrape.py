4fb967deb9a05c5e821fffaf12b99f2e6eefee4f
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-08-15 21:44:45 +0300

    additional improvement

--- hacker_news_scraper/scrape.py
+++ hacker_news_scraper/scrape.py
@@ -2,39 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
-page = requests.get('https://news.ycombinator.com/news?p=1')
-soup = BeautifulSoup(page.text, 'html.parser')
-links = soup.select('.storylink')
-subtext = soup.select('.subtext')
+# page = requests.get('https://news.ycombinator.com/news?p=1')
+# soup = BeautifulSoup(page.text, 'html.parser')
+# links = soup.select('.storylink')
+# subtext = soup.select('.subtext')
+#
+# page1 = requests.get('https://news.ycombinator.com/news?p=2')
+# soup1 = BeautifulSoup(page1.text, 'html.parser')
+# links1 = soup1.select('.storylink')
+# subtext1 = soup1.select('.subtext')
+#
+# page2 = requests.get('https://news.ycombinator.com/news?p=3')
+# soup2 = BeautifulSoup(page2.text, 'html.parser')
+# links2 = soup2.select('.storylink')
+# subtext2 = soup2.select('.subtext')
+#
+# slink = links + links1 + links2
+# ssubtext = subtext + subtext1 + subtext2
 
-page1 = requests.get('https://news.ycombinator.com/news?p=2')
-soup1 = BeautifulSoup(page1.text, 'html.parser')
-links1 = soup1.select('.storylink')
-subtext1 = soup1.select('.subtext')
-
-page2 = requests.get('https://news.ycombinator.com/news?p=3')
-soup2 = BeautifulSoup(page2.text, 'html.parser')
-links2 = soup2.select('.storylink')
-subtext2 = soup2.select('.subtext')
-
-slink = links + links1 + links2
-ssubtext = subtext + subtext1 + subtext2
-
-def link_generator(quantity):
-    counter = 0
-    links_list = []
-    subtext_list = []
-
-    while counter < quantity:
-        page = requests.get(f'https://news.ycombinator.com/news?p={quantity}')
-        soup = BeautifulSoup(page.text, 'html.parser')
-        links = soup.select('.storylink')
-        subtext = soup.select('.subtext')
-        links_list.append(links)
-        subtext_list.append(subtext)
-        counter += 1
-
-    return links_list
 
 def subtext_generator(quantity):
     counter = 0
@@ -57,7 +42,7 @@ def sort_story_by_votes(hnlist):
     return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
-def create_custom_hn(links, subtext):
+def create_custom_hn(links, subtext, votes=100):
     hn = []
 
     for idx, item in enumerate(links):
@@ -67,10 +52,30 @@ def create_custom_hn(links, subtext):
 
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
-            if points > 300:
+            if points > votes:
                 hn.append({'title': title, 'link': href, 'votes': points})
 
     return sort_story_by_votes(hn)
 
 
-pprint.pprint(create_custom_hn(slink, ssubtext))
+def link_generator(quantity):
+    links_list = []
+    subtext_list = []
+    counter = 1
+
+    while counter < quantity:
+        page = requests.get(f'https://news.ycombinator.com/news?p={counter}')
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+        links = soup.select('.storylink')
+        links_list += links
+
+        subtext = soup.select('.subtext')
+        subtext_list += subtext
+
+        counter += 1
+
+    return links_list, subtext_list
+
+
+pprint.pprint(create_custom_hn(link_generator(3)[0], link_generator(3)[1], votes=500))
