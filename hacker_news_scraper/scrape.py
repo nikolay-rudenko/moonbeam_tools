7ab1d56822e28ae22af867edccd74dd55b3541b0
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-08-15 21:51:15 +0300

    additional improvement

--- hacker_news_scraper/scrape.py
+++ hacker_news_scraper/scrape.py
@@ -2,24 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
-# page = requests.get('https://news.ycombinator.com/news?p=1')
-# soup = BeautifulSoup(page.text, 'html.parser')
-# links = soup.select('.storylink')
-# subtext = soup.select('.subtext')
-#
-# page1 = requests.get('https://news.ycombinator.com/news?p=2')
-# soup1 = BeautifulSoup(page1.text, 'html.parser')
-# links1 = soup1.select('.storylink')
-# subtext1 = soup1.select('.subtext')
-#
-# page2 = requests.get('https://news.ycombinator.com/news?p=3')
-# soup2 = BeautifulSoup(page2.text, 'html.parser')
-# links2 = soup2.select('.storylink')
-# subtext2 = soup2.select('.subtext')
-#
-# slink = links + links1 + links2
-# ssubtext = subtext + subtext1 + subtext2
-
 
 def subtext_generator(quantity):
     counter = 0
@@ -53,7 +35,7 @@ def create_custom_hn(links, subtext, votes=100):
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
             if points > votes:
-                hn.append({'title': title, 'link': href, 'votes': points})
+                hn.append({'title': title, 'subtext': href, 'votes': points})
 
     return sort_story_by_votes(hn)
 
@@ -76,6 +58,14 @@ def link_generator(quantity):
         counter += 1
 
     return links_list, subtext_list
+
+
+
+
+class Scrape_Hacker_News(object):
+    def __init__(self, page, votes):
+        self.page = page
+        self.votes = votes
 
 
 pprint.pprint(create_custom_hn(link_generator(3)[0], link_generator(3)[1], votes=500))
