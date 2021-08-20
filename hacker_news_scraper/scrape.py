ec8f268c1dcecc164a20571b73a138bad6ec194a
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-08-20 16:24:53 +0300

    additional improvement

--- hacker_news_scraper/scrape.py
+++ hacker_news_scraper/scrape.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
+
 def sort_story_by_votes(hnlist):
     return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
@@ -47,6 +48,6 @@ class Scrape_Hacker_News(object):
 
         return links_list, subtext_list
 
-sc = Scrape_Hacker_News(page=5,votes=300, quantity=3)
 
+sc = Scrape_Hacker_News(page=5, votes=300, quantity=3)
 pprint.pprint(create_custom_hn(sc.link_generator()[0], sc.link_generator()[1], votes=sc.votes))
